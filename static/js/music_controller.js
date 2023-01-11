bd8285f4376abe2e4220f223c296c68f3674da8c
Joseph <a0926020372@gmail.com>
2023-01-12 07:52:34 +0800

    1/12 7:55

---
+++ static/js/music_controller.js
@@ -0,0 +1,50 @@
+const musicArr = ["../music/piano.wav", "../music/cello.wav", "../music/drum.wav"];
+let _rate = 1;
+let _volume = 0.1;
+
+let piano = new Howl({
+    // 參數設定[註1]
+    src: musicArr[0],
+    autoplay: true,
+    loop: true,
+    volume: _volume,
+    rate: _rate,
+    onload: function () {
+        console.log('Piano Load Ok')
+    }
+});
+let cello = new Howl({
+    // 參數設定[註1]
+    src: musicArr[1],
+    autoplay: true,
+    loop: true,
+    volume: _volume,
+    rate: _rate,
+    onload: function () {
+        console.log('Cello Load Ok')
+    }
+});
+// let drum = new Howl({
+//     // 參數設定[註1]
+//     src: musicArr[2],
+//     autoplay: true,
+//     loop: true,
+//     volume: _volume,
+//     rate: _rate,
+//     onload: function () {
+//         console.log('Drum Load Ok')
+//     }
+// });
+
+function piano_vol(vol) {
+    if(vol<0) {
+        vol = 0;
+        // drum_anim.pause();
+        // cello_anim.pause();
+    }
+    // cello_anim.setSpeed(vol);
+    // drum_anim.setSpeed(vol);
+    cello.volume(vol);
+    console.log(vol);
+}
+
