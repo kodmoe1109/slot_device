deac21aa75e0f0d901bebb18edb37eaa95dd9960
MAC <a0926020372@gmail.com>
2023-03-22 01:07:10 +0800

    新增拉霸機 Serial 檔

---
+++ static/js/serial_cello2.js
@@ -0,0 +1,36 @@
+import SerialPort from "serialport";
+import RLparser from "@serialport/parser-readline";
+import { firebase } from "./firebase.js"
+import { getDatabase, onValue, ref, set, update } from "firebase/database";
+
+const db = getDatabase();//firebase
+const connect_router = ref(db,'/')//firebase
+
+const arduinoPort = 'COM4'
+// const arduinoPort = '/dev/cu.usbserial-14420'
+const port = new SerialPort(arduinoPort, { bauRate: 9600 }, (err) => {
+    if (err) {
+        console.log('fail');
+        return
+    }
+    console.log('已連接至 Arduino UNO 板')
+});
+
+const parser = port.pipe(new RLparser({ delimiter: '\r\n' }))
+
+setTimeout(()=>{serialBegin();},1000)
+function serialBegin() {
+    port.write("open", function () {
+        parser.on("data", function (d) {
+            let data = parseFloat(d)
+            console.log(data);
+            if (data) { // 拉下拉霸
+                let upload_data = data/-10;
+                // console.log(data);
+                update(connect_router, {
+                    'cello_val':upload_data,
+                });
+            }
+        })
+    })
+}
